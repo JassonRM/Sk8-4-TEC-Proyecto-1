0350de50f1714062fe0f77becdf77ed50ea17f80
Marco Herrera <m.herrera0799@gmail.com>
2019-10-08 14:54:46 -0600

    Migrate warehouse tables to PostgreSQL

--- warehouse/schema/table_creation.sql
+++ warehouse/schema/table_creation.sql
@@ -1,66 +1,65 @@
-USE sk8;
 CREATE TABLE IF NOT EXISTS Estado
 (
-    IdEstado    INT AUTO_INCREMENT PRIMARY KEY,
+    IdEstado    SERIAL PRIMARY KEY,
     Descripcion VARCHAR(40) NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS Pais
 (
-    IdPais INT AUTO_INCREMENT PRIMARY KEY,
+    IdPais SERIAL PRIMARY KEY,
     Nombre VARCHAR(40) NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS Provincia
 (
-    IdProvincia INT AUTO_INCREMENT PRIMARY KEY,
+    IdProvincia SERIAL PRIMARY KEY,
     Nombre      VARCHAR(40) NOT NULL,
     IdPais      INT         NOT NULL,
-    CONSTRAINT FkPais
-        FOREIGN KEY (IdPais)
-            REFERENCES Pais (IdPais)
+
+    FOREIGN KEY (IdPais)
+        REFERENCES Pais (IdPais)
 );
 
 
 CREATE TABLE IF NOT EXISTS Canton
 (
-    IdCanton    INT AUTO_INCREMENT PRIMARY KEY,
+    IdCanton    SERIAL PRIMARY KEY,
     Nombre      VARCHAR(40) NOT NULL,
     IdProvincia INT         NOT NULL,
-    CONSTRAINT FkProvincia
-        FOREIGN KEY (IdProvincia)
-            REFERENCES Provincia (IdProvincia)
+
+    FOREIGN KEY (IdProvincia)
+        REFERENCES Provincia (IdProvincia)
 );
 
 
 CREATE TABLE IF NOT EXISTS Distrito
 (
-    IdDistrito INT AUTO_INCREMENT PRIMARY KEY,
+    IdDistrito SERIAL PRIMARY KEY,
     Nombre     VARCHAR(40) NOT NULL,
     IdCanton   INT         NOT NULL,
-    CONSTRAINT FkCanton
-        FOREIGN KEY (IdCanton)
-            REFERENCES Canton (IdCanton)
+
+    FOREIGN KEY (IdCanton)
+        REFERENCES Canton (IdCanton)
 );
 
 
 CREATE TABLE IF NOT EXISTS Direccion
 (
-    IdDireccion INT AUTO_INCREMENT PRIMARY KEY,
+    IdDireccion SERIAL PRIMARY KEY,
     IdDistrito  INT          NOT NULL,
     Detalle1    VARCHAR(100) NOT NULL,
     Detalle2    VARCHAR(100),
-    CONSTRAINT FkDistrito
-        FOREIGN KEY (IdDistrito)
-            REFERENCES Distrito (IdDistrito)
+
+    FOREIGN KEY (IdDistrito)
+        REFERENCES Distrito (IdDistrito)
 );
 
 
 CREATE TABLE IF NOT EXISTS Persona
 (
-    IdPersona       INT AUTO_INCREMENT PRIMARY KEY,
+    IdPersona       SERIAL PRIMARY KEY,
     Identificacion  VARCHAR(40) UNIQUE NOT NULL,
     Nombre          VARCHAR(40)        NOT NULL,
     Apellido1       VARCHAR(40)        NOT NULL,
@@ -71,41 +70,41 @@ CREATE TABLE IF NOT EXISTS Persona
     FechaRegistro   DATE               NOT NULL,
     IdEstado        INT                NOT NULL,
     IdDireccion     INT                NOT NULL,
-    CONSTRAINT FkEstado
-        FOREIGN KEY (IdEstado)
-            REFERENCES Estado (IdEstado),
-    CONSTRAINT FkDireccion
-        FOREIGN KEY (IdDireccion)
-            REFERENCES Direccion (IdDireccion)
+
+    FOREIGN KEY (IdEstado)
+        REFERENCES Estado (IdEstado),
+
+    FOREIGN KEY (IdDireccion)
+        REFERENCES Direccion (IdDireccion)
 );
 
 
 CREATE TABLE IF NOT EXISTS Cliente
 (
-    IdCliente   INT AUTO_INCREMENT PRIMARY KEY,
+    IdCliente   SERIAL PRIMARY KEY,
     IdPersona   INT NOT NULL,
     Descripcion VARCHAR(100),
     Puntos      INT NOT NULL,
-    CONSTRAINT FkPersona
-        FOREIGN KEY (IdPersona)
-            REFERENCES Persona (IdPersona)
+
+    FOREIGN KEY (IdPersona)
+        REFERENCES Persona (IdPersona)
 );
 
 
 CREATE TABLE IF NOT EXISTS Empleado
 (
-    IdEmpleado  INT AUTO_INCREMENT PRIMARY KEY,
+    IdEmpleado  SERIAL PRIMARY KEY,
     IdPersona   INT NOT NULL,
     Descripcion VARCHAR(100),
-    CONSTRAINT FkPersona
-        FOREIGN KEY (IdPersona)
-            REFERENCES Persona (IdPersona)
+
+    FOREIGN KEY (IdPersona)
+        REFERENCES Persona (IdPersona)
 );
 
 
 CREATE TABLE IF NOT EXISTS Puesto
 (
-    IdPuesto    INT AUTO_INCREMENT PRIMARY KEY,
+    IdPuesto    SERIAL PRIMARY KEY,
     Nombre      VARCHAR(40) NOT NULL,
     Descripcion VARCHAR(100)
 );
@@ -119,24 +118,24 @@ CREATE TABLE IF NOT EXISTS EmpleadoPuesto
     Salario    INT  NOT NULL,
     Fecha      DATE NOT NULL,
     IdEstado   INT  NOT NULL,
-    CONSTRAINT FkEmpleado
-        FOREIGN KEY (IdEmpleado)
-            REFERENCES Empleado (IdEmpleado),
-    CONSTRAINT FkPuesto
-        FOREIGN KEY (IdPuesto)
-            REFERENCES Puesto (IdPuesto),
-    CONSTRAINT FkSucursal
-        FOREIGN KEY (IdSucursal)
-            REFERENCES Sucursal (IdSucursal),
-    CONSTRAINT FkEstado
-        FOREIGN KEY (IdEstado)
-            REFERENCES Estado (IdEstado)
+
+    FOREIGN KEY (IdEmpleado)
+        REFERENCES Empleado (IdEmpleado),
+
+    FOREIGN KEY (IdPuesto)
+        REFERENCES Puesto (IdPuesto),
+
+    FOREIGN KEY (IdSucursal)
+        REFERENCES Sucursal (IdSucursal),
+
+    FOREIGN KEY (IdEstado)
+        REFERENCES Estado (IdEstado)
 );
 
 
 CREATE TABLE IF NOT EXISTS Categoria
 (
-    IdCategoria INT AUTO_INCREMENT PRIMARY KEY,
+    IdCategoria SERIAL PRIMARY KEY,
     Nombre      VARCHAR(40) NOT NULL,
     Descripcion VARCHAR(100)
 );
@@ -144,7 +143,7 @@ CREATE TABLE IF NOT EXISTS Categoria
 
 CREATE TABLE IF NOT EXISTS SKU
 (
-    IdSKU            INT AUTO_INCREMENT PRIMARY KEY,
+    IdSKU            SERIAL PRIMARY KEY,
     Codigo           VARCHAR(40) NOT NULL,
     Nombre           VARCHAR(40) NOT NULL,
     Descripcion      VARCHAR(100),
@@ -155,29 +154,29 @@ CREATE TABLE IF NOT EXISTS SKU
     FechaCaducidad   DATE        NOT NULL,
     DetalleUbicacion VARCHAR(100),
     Cantidad         INT         NOT NULL,
-    CONSTRAINT FkCategoria
-        FOREIGN KEY (IdCategoria)
-            REFERENCES Categoria (IdCategoria),
-    CONSTRAINT FkEstado
-        FOREIGN KEY (IdEstado)
-            REFERENCES Estado (IdEstado)
+
+    FOREIGN KEY (IdCategoria)
+        REFERENCES Categoria (IdCategoria),
+
+    FOREIGN KEY (IdEstado)
+        REFERENCES Estado (IdEstado)
 );
 
 
 CREATE TABLE IF NOT EXISTS Sucursal
 (
-    IdSucursal  INT AUTO_INCREMENT PRIMARY KEY,
+    IdSucursal  SERIAL PRIMARY KEY,
     Codigo      VARCHAR(40) NOT NULL,
     Nombre      VARCHAR(40) NOT NULL,
     Descripcion VARCHAR(100),
     IdDireccion INT         NOT NULL,
     IdEstado    INT         NOT NULL,
-    CONSTRAINT FkEstado
-        FOREIGN KEY (IdEstado)
-            REFERENCES Estado (IdEstado),
-    CONSTRAINT FkDireccion
-        FOREIGN KEY (IdDireccion)
-            REFERENCES Direccion (IdDireccion)
+
+    FOREIGN KEY (IdEstado)
+        REFERENCES Estado (IdEstado),
+
+    FOREIGN KEY (IdDireccion)
+        REFERENCES Direccion (IdDireccion)
 );
 
 
@@ -185,47 +184,47 @@ CREATE TABLE IF NOT EXISTS SucursalSKU
 (
     IdSucursal INT NOT NULL,
     IdSKU      INT NOT NULL,
-    CONSTRAINT FkSucursal
-        FOREIGN KEY (IdSucursal)
-            REFERENCES Sucursal (IdSucursal),
-    CONSTRAINT FkSKU
-        FOREIGN KEY (IdSKU)
-            REFERENCES SKU (IdSKU)
+
+    FOREIGN KEY (IdSucursal)
+        REFERENCES Sucursal (IdSucursal),
+
+    FOREIGN KEY (IdSKU)
+        REFERENCES SKU (IdSKU)
 );
 
 
 CREATE TABLE IF NOT EXISTS Camion
 (
-    IdCamion    INT AUTO_INCREMENT PRIMARY KEY,
+    IdCamion    SERIAL PRIMARY KEY,
     Placa       VARCHAR(20) NOT NULL,
     Descripcion VARCHAR(100),
     IdEstado    INT         NOT NULL,
-    CONSTRAINT FkEstado
-        FOREIGN KEY (IdEstado)
-            REFERENCES Estado (IdEstado)
+
+    FOREIGN KEY (IdEstado)
+        REFERENCES Estado (IdEstado)
 );
 
 
 CREATE TABLE IF NOT EXISTS Envio
 (
-    IdEnvio     INT AUTO_INCREMENT PRIMARY KEY,
-    IdCamion    INT      NOT NULL,
-    IdChofer    INT      NOT NULL,
-    IdEncargado INT      NOT NULL,
-    IdSucursal  INT      NOT NULL,
-    Fecha       DATETIME NOT NULL,
-    CONSTRAINT FkCamion
-        FOREIGN KEY (IdCamion)
-            REFERENCES Camion (IdCamion),
-    CONSTRAINT FkChofer
-        FOREIGN KEY (IdChofer)
-            REFERENCES Empleado (IdEmpleado),
-    CONSTRAINT FkEncargado
-        FOREIGN KEY (IdEncargado)
-            REFERENCES Empleado (IdEmpleado),
-    CONSTRAINT FkSucursal
-        FOREIGN KEY (IdSucursal)
-            REFERENCES Sucursal (IdSucursal)
+    IdEnvio     SERIAL PRIMARY KEY,
+    IdCamion    INT       NOT NULL,
+    IdChofer    INT       NOT NULL,
+    IdEncargado INT       NOT NULL,
+    IdSucursal  INT       NOT NULL,
+    Fecha       TIMESTAMP NOT NULL,
+
+    FOREIGN KEY (IdCamion)
+        REFERENCES Camion (IdCamion),
+
+    FOREIGN KEY (IdChofer)
+        REFERENCES Empleado (IdEmpleado),
+
+    FOREIGN KEY (IdEncargado)
+        REFERENCES Empleado (IdEmpleado),
+
+    FOREIGN KEY (IdSucursal)
+        REFERENCES Sucursal (IdSucursal)
 );
 
 
@@ -234,12 +233,12 @@ CREATE TABLE IF NOT EXISTS EnvioPaquete
     IdEnvio  INT NOT NULL,
     IdSKU    INT NOT NULL,
     Cantidad INT NOT NULL,
-    CONSTRAINT FkEnvio
-        FOREIGN KEY (IdEnvio)
-            REFERENCES Envio (IdEnvio),
-    CONSTRAINT FkSKU
-        FOREIGN KEY (IdSKU)
-            REFERENCES SKU (IdSKU)
+
+    FOREIGN KEY (IdEnvio)
+        REFERENCES Envio (IdEnvio),
+
+    FOREIGN KEY (IdSKU)
+        REFERENCES SKU (IdSKU)
 );
 
 
@@ -249,46 +248,46 @@ CREATE TABLE IF NOT EXISTS PedidoPaquete
     IdSKU    INT NOT NULL,
     Cantidad INT NOT NULL,
     Costo    INT NOT NULL,
-    CONSTRAINT FkPedido
-        FOREIGN KEY (IdPedido)
-            REFERENCES Pedido (IdPedido),
-    CONSTRAINT FkSKU
-        FOREIGN KEY (IdSKU)
-            REFERENCES SKU (IdSKU)
+
+    FOREIGN KEY (IdPedido)
+        REFERENCES Pedido (IdPedido),
+
+    FOREIGN KEY (IdSKU)
+        REFERENCES SKU (IdSKU)
 );
 
 
 CREATE TABLE IF NOT EXISTS Proveedor
 (
-    IdProveedor INT AUTO_INCREMENT PRIMARY KEY,
+    IdProveedor SERIAL PRIMARY KEY,
     Nombre      VARCHAR(40) NOT NULL,
     Descripcion VARCHAR(100),
     IdEstado    INT         NOT NULL,
-    CONSTRAINT FkEstado
-        FOREIGN KEY (IdEstado)
-            REFERENCES Estado (IdEstado)
+
+    FOREIGN KEY (IdEstado)
+        REFERENCES Estado (IdEstado)
 );
 
 
 CREATE TABLE IF NOT EXISTS Pedido
 (
-    IdPedido    INT AUTO_INCREMENT PRIMARY KEY,
+    IdPedido    SERIAL PRIMARY KEY,
     Fecha       DATE NOT NULL,
     Descripcion VARCHAR(100),
     IdProveedor INT  NOT NULL,
     IdEncargado INT  NOT NULL,
-    CONSTRAINT FkProveedor
-        FOREIGN KEY (IdProveedor)
-            REFERENCES Proveedor (IdProveedor),
-    CONSTRAINT FkEncargado
-        FOREIGN KEY (IdEncargado)
-            REFERENCES Empleado (IdEmpleado)
+
+    FOREIGN KEY (IdProveedor)
+        REFERENCES Proveedor (IdProveedor),
+
+    FOREIGN KEY (IdEncargado)
+        REFERENCES Empleado (IdEmpleado)
 );
 
 
 CREATE TABLE IF NOT EXISTS MetodoPago
 (
-    IdMetodoPago INT AUTO_INCREMENT PRIMARY KEY,
+    IdMetodoPago SERIAL PRIMARY KEY,
     Metodo       VARCHAR(40) NOT NULL,
     Descripcion  VARCHAR(100)
 );
@@ -296,20 +295,20 @@ CREATE TABLE IF NOT EXISTS MetodoPago
 
 CREATE TABLE IF NOT EXISTS Promocion
 (
-    IdPromocion INT AUTO_INCREMENT PRIMARY KEY,
-    IdSKU       INT      NOT NULL,
-    Inicio      DATETIME NOT NULL,
-    Fin         DATETIME NOT NULL,
-    Descuento   INT      NOT NULL,
-    CONSTRAINT FkSKU
-        FOREIGN KEY (IdSKU)
-            REFERENCES SKU (IdSKU)
+    IdPromocion SERIAL PRIMARY KEY,
+    IdSKU       INT       NOT NULL,
+    Inicio      TIMESTAMP NOT NULL,
+    Fin         TIMESTAMP NOT NULL,
+    Descuento   INT       NOT NULL,
+
+    FOREIGN KEY (IdSKU)
+        REFERENCES SKU (IdSKU)
 );
 
 
 CREATE TABLE IF NOT EXISTS Factura
 (
-    IdFactura       INT AUTO_INCREMENT PRIMARY KEY,
+    IdFactura       SERIAL PRIMARY KEY,
     Codigo          VARCHAR(40) NOT NULL,
     Fecha           DATE        NOT NULL,
     SubTotal        INT         NOT NULL,
@@ -320,30 +319,30 @@ CREATE TABLE IF NOT EXISTS Factura
     IdCliente       INT         NOT NULL,
     IdEmpleado      INT         NOT NULL,
     IdMetodoPago    INT         NOT NULL,
-    CONSTRAINT FkSucursal
-        FOREIGN KEY (IdSucursal)
-            REFERENCES Sucursal (IdSucursal),
-    CONSTRAINT FkCliente
-        FOREIGN KEY (IdCliente)
-            REFERENCES Cliente (IdCliente),
-    CONSTRAINT FkEmpleado
-        FOREIGN KEY (IdEmpleado)
-            REFERENCES Empleado (IdEmpleado),
-    CONSTRAINT FkMetodoPago
-        FOREIGN KEY (IdMetodoPago)
-            REFERENCES MetodoPago (IdMetodoPago)
+
+    FOREIGN KEY (IdSucursal)
+        REFERENCES Sucursal (IdSucursal),
+
+    FOREIGN KEY (IdCliente)
+        REFERENCES Cliente (IdCliente),
+
+    FOREIGN KEY (IdEmpleado)
+        REFERENCES Empleado (IdEmpleado),
+
+    FOREIGN KEY (IdMetodoPago)
+        REFERENCES MetodoPago (IdMetodoPago)
 );
 
 CREATE TABLE IF NOT EXISTS PromocionFactura
 (
     IdPromocion INT NOT NULL,
     IdFactura   INT NOT NULL,
-    CONSTRAINT FkPromocion
-        FOREIGN KEY (IdPromocion)
-            REFERENCES Promocion (IdPromocion),
-    CONSTRAINT FkFactura
-        FOREIGN KEY (IdFactura)
-            REFERENCES Factura (IdFactura)
+
+    FOREIGN KEY (IdPromocion)
+        REFERENCES Promocion (IdPromocion),
+
+    FOREIGN KEY (IdFactura)
+        REFERENCES Factura (IdFactura)
 );
 
 CREATE TABLE IF NOT EXISTS Venta
@@ -352,10 +351,10 @@ CREATE TABLE IF NOT EXISTS Venta
     IdFactura INT NOT NULL,
     Cantidad  INT NOT NULL,
     Precio    INT NOT NULL,
-    CONSTRAINT FkSKU
-        FOREIGN KEY (IdSKU)
-            REFERENCES SKU (IdSKu),
-    CONSTRAINT FkFactura
-        FOREIGN KEY (IdFactura)
-            REFERENCES Factura (IdFactura)
+
+    FOREIGN KEY (IdSKU)
+        REFERENCES SKU (IdSKu),
+
+    FOREIGN KEY (IdFactura)
+        REFERENCES Factura (IdFactura)
 );

ec0757e91adb2358a3a3790d0a2df8cfa2af45ca
Jasson Rodríguez <jassonrm@icloud.com>
2019-10-11 04:25:13 -0600

    Add seed for metodopago

---
+++ warehouse/schema/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO metodopago (metodo, descripcion)
+VALUES
+       ('Master Card', 'Tarjeta de la marca Master Card'),
+       ('Visa', 'Tarjeta de la marca Visa'),
+       ('Efectivo', 'Los viejos billetes'),
+       ('Puntos', 'Canje de puntos de cliente frecuente');

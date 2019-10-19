ac77ad14dd5d2f1fd6c7c704161ef0cf2be62413
Marco Herrera <m.herrera0799@gmail.com>
2019-10-19 01:18:51 -0600

    Update output from procedures

--- branch/queries/reports.sql
+++ branch/queries/reports.sql
@@ -1,98 +1,4 @@
-DELIMITER //
-CREATE PROCEDURE EmpleadoMes(IN Mes INT, IN Año INT, OUT Nombre VARCHAR(40))
-BEGIN
-    SELECT P.Nombre
-    FROM Factura F
-             INNER JOIN Empleado E
-                        ON F.IdEmpleado = E.IdEmpleado
-             INNER JOIN Persona P ON E.IdPersona = P.IdPersona
-    WHERE MONTH(F.Fecha) = Mes
-      AND YEAR(F.Fecha) = Año
-    GROUP BY F.IdEmpleado
-    ORDER BY SUM(F.SubTotal) DESC
-    LIMIT 1
-    INTO Nombre;
-END //
-DELIMITER ;
-
-
-DELIMITER //
-CREATE PROCEDURE CierreCajaFacturas(IN date VARCHAR(20))
-BEGIN
-    SELECT F.*
-    FROM Factura F
-    WHERE F.Fecha = date;
-END
-//
-DELIMITER ;
-
-
-DELIMITER //
-CREATE PROCEDURE CierreCajaVentas(IN date VARCHAR(20))
-BEGIN
-    SELECT V.*
-    FROM Venta V
-             INNER JOIN Factura F ON V.IdFactura = F.IdFactura
-    WHERE F.Fecha = date;
-END
-//
-DELIMITER ;
-
-
-DELIMITER //
-CREATE PROCEDURE CierreCajaPromociones()
-BEGIN
-    SELECT P.*
-    FROM Promocion P
-    WHERE DATE(P.Inicio) = CURRENT_DATE(); -- TODO Revisar como mejorar desempeño
-END
-//
-DELIMITER ;
-
-DELIMITER //
-CREATE PROCEDURE CierreCajaPromocionFactura(IN date VARCHAR(20))
-BEGIN
-    SELECT PF.*
-    FROM PromocionFactura PF
-             INNER JOIN Factura F ON PF.IdFactura = F.IdFactura
-    WHERE F.Fecha = date;
-END
-//
-DELIMITER ;
-
-
-DELIMITER //
-CREATE PROCEDURE GarantiaArticulo(IN IdArticulo INT)
-BEGIN
-    SELECT DATE_ADD(F.Fecha, INTERVAL S.Garantia MONTH) AS Garantía
-    FROM Factura F
-             INNER JOIN Venta V ON F.IdFactura = V.IdFactura
-             INNER JOIN Articulo A ON V.IdArticulo = A.IdArticulo
-             INNER JOIN SKU S ON A.IdSKU = S.IdSKU
-    WHERE A.IdArticulo = IdArticulo;
-END
-//
-DELIMITER ;
-
-
-DELIMITER //
-CREATE PROCEDURE InicioPromocion(IN Fecha DATETIME, OUT Descuento INT)
-BEGIN
-    SELECT P.Descuento FROM Promocion P WHERE Inicio = Fecha INTO Descuento;
-END
-//
-DELIMITER ;
-
-
-DELIMITER //
-CREATE PROCEDURE FinPromocion(IN Fecha DATETIME, OUT Descuento INT)
-BEGIN
-    SELECT P.Descuento FROM Promocion P WHERE Fin = Fecha INTO Descuento;
-END
-//
-DELIMITER ;
-
-
+# Gets the amount of Articulos in each state
 DELIMITER //
 CREATE PROCEDURE ReporteProductos()
 BEGIN
@@ -106,6 +12,7 @@ END
 DELIMITER ;
 
 
+# Gets all the money sold (without taxes) and the amount of Ventas per year
 DELIMITER //
 CREATE PROCEDURE ReporteCompras()
 BEGIN
